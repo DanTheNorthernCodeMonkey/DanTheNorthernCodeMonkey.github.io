c2c46f71483bfaaeb1303f02ad7c5c7e38289217
DanTheNorthernCodeMonkey <dan.ian.shea@gmail.com>
2016-08-21 21:07:15 +0100

    sw

--- sw.js
+++ sw.js
@@ -1,87 +1,83 @@
+(function () {
 
-    var self = this;
-
-    // These cache names need incrementing on changes happening, make part of a build script.
-    var appShellCacheName = 'appShell-v1.0',
-        dataCacheName = 'dataCache-v1.0',
-        siteFullDomain = 'https://www.danthenortherncodemonkey.com',
+	var self = this;
+	// These cache names need incrementing on changes happening, make part of a build script.
+	var cacheName = 'appShellV1',
         appShellFiles = [
             // CDNd files, this is fucking horrible, cloudflare CDNs my assets anyway anyway.
             // TODO: Make a gulp build script to consolidate all CSS into one file and all js into one file.
             //'/offline/index.html',
-            '/'
+            'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
+            'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css',
+            '/css/rrssb.css',
+            '/css/grayscale.css',
+            '/css/timeline.css',
+            "/"
         ];
-     
 
-    self.addEventListener('install', function(event) {
-        console.log('Started', self);
 
-        // Older service workers will cause this one to "wait". This skips the waiting stage.
-        self.skipWaiting();
+	self.addEventListener('install', function (event) {
+		console.log('Started', self);
 
-        console.log('Installed', event);
-
-        function onInstall () {
-            return caches.open('appShell')
+		function onInstall() {
+			return caches.open(cacheName)
                 .then(function (cache) {
-                    cache.addAll(appShellFiles); // Atomic, one fails, it all fails
-                });
-        }
+                	cache.addAll(appShellFiles); // Atomic, one fails, it all fails
+                }).then(self.skipWaiting()); // Older service workers will cause this one to "wait". This skips the waiting stage.
+		}
+		event.waitUntil(onInstall(event));
+	});
 
-        event.waitUntil(onInstall(event));
-    });
-    
-    self.addEventListener('activate', function(e) {
-        console.log('[ServiceWorker] Activate');
-        e.waitUntil(
-            // caches.keys().then(function(keyList) {
-            //     // Flushing the old cache here
-            //     return Promise.all(keyList.map(function(key) {
-            //         console.log('[ServiceWorker] Removing old cache', key);
-            //         if (key !== appShellCacheName) {
-            //             return caches.delete(key);
-            //         }
-            //     }));
-            // })
-        );
-    });
-
-    self.addEventListener('fetch', function(e) {
-
-        var fetchRequest = e.request.clone();
-
-        console.log('[ServiceWorker] Fetch', fetchRequest.url);
-        // if (isNotInAppShellCache(fetchRequest)) {
-        //   e.respondWith(
-        //     fetch(fetchRequest)
-        //       .then(function(response) {
-        //         return caches.open(dataCacheName).then(function(cache) {
-                
-        //           if (!response || response.status !== 200) // On error return the offline page.
-        //             caches.match('/').then(function (response) {
-        //                 return response;
-        //             });
-
-
-        //           cache.put(fetchRequest.url, response.clone());
-        //           console.log('[ServiceWorker] Fetched & Cached Data');
-        //           return response;
-        //         });
-        //       })
-        //   );
-        // } else {
-          e.respondWith(
-            caches.match(fetchRequest).then(function(response) {
-              return response || fetch(fetchRequest);
+	self.addEventListener('activate', function (e) {
+		console.log('[ServiceWorker] Activate');
+		e.waitUntil(
+            caches.keys().then(function (keyList) {
+            	// Flushing the old cache here
+            	// As it's a static site, a post is the only change, which also updates the index, therefore
+            	// updating the cache name via versioning will remove the old cache and replace with the new.
+            	return Promise.all(keyList.map(function (key) {
+            		console.log('[ServiceWorker] Removing old cache', key);
+            		if (key !== cacheName) {
+            			return caches.delete(key);
+            		}
+            	}));
+            }).catch(function (e) {
+            	console.log('[ServiceWorker] Error removing old cache, error message: ' + e);
             })
-          );
-        // }
-    });
+        );
+	});
 
-    function isNotInAppShellCache (url) {
-        cache.open(appShellCacheName).then(function (cache) {
-            caches.match(url).then(function (response) {
-                return !response ? true : false
-            });
-        });
-    }
+	// As the site is static and will not use a proper "App cache" I've opted for 
+	// a simple try cache, if not get then cache strategy. A more complex app will need
+	// a much more complex caching strategy, e.g. per file type, per domain, request type etc.
+	self.addEventListener('fetch', function (e) {
+
+		var fetchRequest = e.request.clone();
+		console.log('[ServiceWorker] Fetch', fetchRequest.url);
+		e.respondWith(
+            caches.match(fetchRequest).then(function (response) {
+
+            	// If cached return straight away
+            	if (response)
+            		return response;
+
+            	// If not get it, then cache it
+            	return fetch(fetchRequest).then(function (response) {
+            		return caches.open(cacheName).then(function (cache) {
+
+            			// On error return the offline page.
+            			if (!response || response.status !== 200) {
+            				caches.match('/').then(function (response) {
+            					return response;
+            				});
+            			}
+
+            			cache.put(fetchRequest.url, response.clone());
+            			console.log('[ServiceWorker] Fetched & Cached Data');
+            			return response;
+            		});
+            	});
+            })
+        );
+	});
+}());
